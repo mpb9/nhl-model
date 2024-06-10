5c2b9306cc2fabc8b79f5268b11f39c375da79b4
mpb9 <michaelbeebe1031@gmail.com>
2024-06-10 06:29:39 -0400

    initialization - not working or tested if it did

--- api_config/__init__.py
+++ api_config/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
-from api_config import pages
+from flask_restplus import Api, Resource
+from flask_swagger_ui import get_swaggerui_blueprint
+
+# from api_config import pages
+
+# MARK: Swagger UI
+SWAGGER_URL = "/swagger"
+API_URL = "http://127.0.0.1:8080/swagger.json"
 
 
 def create_app() -> Flask:
     app = Flask(import_name=__name__)
-    app.register_blueprint(blueprint=pages.bp)
-    return app
+    # app.register_blueprint(blueprint=pages.bp)
+    # return app
+    api = Api(app=app)
